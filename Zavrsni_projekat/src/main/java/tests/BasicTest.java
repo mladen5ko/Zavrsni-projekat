d079bfc18f292c4b3bfec1c2e394c4d583b73438
Mladen Petkovic <mladenpetkovic991@gmail.com>
2022-06-23 18:24:11 +0200

    Minor uprate add aftermethod/screenshot

--- Zavrsni_projekat/src/main/java/tests/BasicTest.java
+++ Zavrsni_projekat/src/main/java/tests/BasicTest.java
@@ -1,10 +1,18 @@
 package tests;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.*;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import pages.*;
 
+import java.io.File;
 import java.time.Duration;
 
 public abstract class BasicTest {
@@ -37,6 +45,19 @@ public abstract class BasicTest {
     @BeforeMethod
     public void beforeMethod() {
         driver.get(baseUrl);
+    }
+    @AfterMethod
+    public void takeScreenshot(ITestResult result) {
+        if (ITestResult.FAILURE == result.getStatus()) {
+            try {
+                TakesScreenshot ts = (TakesScreenshot) driver;
+                File source = ts.getScreenshotAs(OutputType.FILE);
+                FileHandler.copy(source, new File("src/main/java/Fail test screenshot/" + result.getName() + ".png"));
+                System.out.println("Screenshot taken");
+            } catch (Exception e) {
+                System.out.println("Exception while taking screenshot " + e.getMessage());
+            }
+        }
     }
 
     @AfterClass
